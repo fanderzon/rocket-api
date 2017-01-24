29e4ead2a16333d866666746ae4855a4ef03d1d4
Fredrik Andersson <fredrik.andersson@widespace.com>
2017-01-24 23:44:56 +0100

    Removes unused routes, and made note creation return the created Note object

--- src/main.rs
+++ src/main.rs
@@ -24,36 +24,19 @@ use note::{get_notes, create_note};
 use models::*;
 use rocket_contrib::JSON;
 
-#[get("/hello/<name>/<age>")]
-fn hello(name: &str, age: u8) -> String {
-    format!("Hello, {} year old named {}!", age, name)
-}
-
 #[get("/note", format = "application/json")]
 fn notes_get() -> Option<JSON<Vec<Note>>> {
-    let notes = get_notes();
+    let connection = establish_connection();
+    let notes = get_notes(&connection);
     Some(JSON(notes))
 }
 
 #[post("/note", format = "application/json", data = "<note>")]
-fn note_create(note: NewNote) -> String {
+fn note_create(note: NewNote) -> JSON<Note> {
     let connection = establish_connection();
-    let note = create_note(&connection, note);
-    return note.title;
-}
-
-#[get("/hello/<name>")]
-fn hi<'r>(name: &'r str) -> &'r str {
-    let results = get_notes();
-    println!("Displaying {} notes", results.len());
-    for note in results {
-        println!("{}", note.title);
-        println!("----------\n");
-        println!("{}", note.body);
-    }
-    name
+    JSON(create_note(&connection, note))
 }
 
 fn main() {
-    rocket::ignite().mount("/", routes![hello, hi, note_create, notes_get]).launch();
+    rocket::ignite().mount("/", routes![note_create, notes_get]).launch();
 }
--- src/note.rs
+++ src/note.rs
@@ -1,26 +1,19 @@
 use diesel;
 use diesel::pg::PgConnection;
-use db::*;
 use models::*;
 use diesel::prelude::*;
 
-pub fn get_notes() -> Vec<Note> {
+pub fn get_notes(conn: &PgConnection) -> Vec<Note> {
     use schema::notes::dsl::*;
 
-    let connection = establish_connection();
     notes.filter(pinned.eq(false))
         .limit(5)
-        .load::<Note>(&connection)
+        .load::<Note>(conn)
         .expect("Error loading notes")
 }
 
 pub fn create_note(conn: &PgConnection, note: NewNote) -> Note {
     use schema::notes;
-
-    // let new_note = NewNote {
-    //     title: "Hello".to_owned(),
-    //     body: "World".to_owned(),
-    // };
 
     diesel::insert(&note)
         .into(notes::table)
